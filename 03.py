2164c6a268be76e7a0605eff4f9ae477d0c54dc4
Stash <jstash@gmail.com>
2024-12-03 22:14:37 -0800

    03 part 2

--- 03.py
+++ 03.py
@@ -1,17 +1,24 @@
 import re
-import io
-import os
 
 
 def sum_expr(s):
     sum = 0
-    for m in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", s):
-        sum += int(m[0]) * int(m[1])
+    while len(s) > 0:
+        try:
+            (valid, s) = s.split("don't()", 1)
+            try:
+                (_,s) = s.split("do()", 1)
+            except ValueError:
+                s = "" # no trailing do block
+        except ValueError:
+            (valid, s) = (s,"")
+        for m in re.findall(r"mul\((\d{1,3}),(\d{1,3})\)", valid):
+            sum += int(m[0]) * int(m[1])
     return sum
 
-test = "xmul(2,4)%&mul[3,7]!@^do_not_mul(5,5)+mul(32,64]then(mul(11,8)mul(8,5))"
-print(sum_expr(test))
+test = "xmul(2,4)&mul[3,7]!^don't()\n_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))"
+print(sum_expr(f"{test}".replace("\n"," ")))
 
-with open("inputs/03p1.txt") as f:
+with open("inputs/03p2.txt") as f:
     chal = f.read().replace("\n"," ")
-print(sum_expr(chal))
+print(sum_expr(f"{chal}"))
