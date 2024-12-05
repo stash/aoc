ffd764476f54abaffe0087d25b0faa01255059fb
Jeremy Stashewsky <jstash@gmail.com>
2024-12-04 20:26:54 -0800

    01 part 2 in rust

--- src/day01.rs
+++ src/day01.rs
@@ -22,15 +22,34 @@ pub fn part1(lines: Vec<String>) -> Result<String> {
 }
 
 pub fn part2(lines: Vec<String>) -> Result<String> {
+    let mut a: Vec<usize> = Vec::new();
+    let mut b: Vec<usize> = Vec::new();
+
     for line in lines {
-        println!("{}", line)
+        let parts: Vec<&str> = line.split_ascii_whitespace().collect();
+        if parts.len() > 2 {
+            bail!("line too long")
+        }
+        a.push(parts[0].parse()?);
+        b.push(parts[1].parse()?);
+        //println!("{}", parts.join(","))
     }
-    bail!("Part2 fail!")
+    let mut total: usize = 0;
+    for x in &a {
+        let mut count: usize = 0;
+        for y in &b {
+            if x == y {
+                count += 1
+            }
+        }
+        total += x * count;
+    }
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
 mod test {
-    use anyhow::{Ok, Result};
+    use anyhow::Result;
     use indoc::indoc;
 
     use super::*;
@@ -50,6 +69,23 @@ mod test {
         .collect();
 
         assert_eq!(part1(input)?, "11");
+        Ok(())
+    }
+
+    #[test]
+    pub fn test_part2() -> Result<()> {
+        let input: Vec<String> = indoc! {"
+            3   4
+            4   3
+            2   5
+            1   3
+            3   9
+            3   3
+        "}
+        .lines()
+        .map(|x| x.to_string())
+        .collect();
+        assert_eq!(part2(input)?, "31");
         Ok(())
     }
 }
