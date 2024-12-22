cb34db4927050dc81192e55d0bfe4bcdfdefe407
Jeremy Stashewsky <jstash@gmail.com>
2024-12-21 19:15:12 -0800

    day 21 part 2 in rust
    an ode to `match` and `memoize`

--- src/day21.rs
+++ src/day21.rs
@@ -1,7 +1,8 @@
 use anyhow::{bail, Result};
 use itertools::Itertools;
+use memoize::memoize;
 
-use crate::common::{Dir, Pos};
+use crate::common::Pos;
 type Point = Pos<isize>;
 
 static KP_A: Point = Point { x: 2, y: 3 };
@@ -16,39 +17,13 @@ static KP_7: Point = Point { x: 0, y: 0 };
 static KP_8: Point = Point { x: 1, y: 0 };
 static KP_9: Point = Point { x: 2, y: 0 };
 
-static DP_A: Point = Point { x: 2, y: 0 };
-static DP_U: Point = Point { x: 1, y: 0 };
-static DP_L: Point = Point { x: 0, y: 1 };
-static DP_D: Point = Point { x: 1, y: 1 };
-static DP_R: Point = Point { x: 2, y: 1 };
-
-#[derive(Clone, Copy, Debug, PartialEq, Eq, Hash)]
+#[derive(Clone, Copy, PartialEq, Eq, Hash)]
 enum DP {
     A,
     U,
     L,
     D,
     R,
-}
-impl DP {
-    fn to_char(&self) -> char {
-        match self {
-            DP::A => 'A',
-            DP::U => '^',
-            DP::L => '<',
-            DP::D => 'v',
-            DP::R => '>',
-        }
-    }
-    fn to_point(&self) -> Point {
-        match self {
-            DP::A => DP_A,
-            DP::U => DP_U,
-            DP::L => DP_L,
-            DP::D => DP_D,
-            DP::R => DP_R,
-        }
-    }
 }
 
 fn gen_raw_steps(from: Point, to: Point) -> Vec<DP> {
@@ -63,30 +38,23 @@ fn gen_raw_steps(from: Point, to: Point) -> Vec<DP> {
     raw_steps
 }
 
-fn shortest_encoding<F>(alts: Vec<Vec<DP>>, encode: F) -> Result<Vec<DP>>
+fn shortest_encoding<F>(alts: Vec<Vec<DP>>, encode: F) -> usize
 where
-    F: Fn(Vec<DP>) -> Result<Vec<DP>>,
+    F: Fn(Vec<DP>) -> usize,
 {
-    let mut iter = alts.into_iter();
-    let mut shortest: Vec<DP> = encode(iter.next().unwrap())?;
-    for alt in iter {
-        let encoding = encode(alt)?;
-        if encoding.len() < shortest.len() {
-            shortest = encoding;
-        }
-    }
-    Ok(shortest)
+    alts.into_iter()
+        .map(encode)
+        .min()
+        .expect("at least one alt")
 }
 
-fn encode_dp(dps: Vec<DP>, depth: usize) -> Result<Vec<DP>> {
-    if depth == 0 {
-        return Ok(dps);
-    }
-    println!(" encoding d{} {:?}", depth, dps);
-    let mut next_dps = vec![];
+#[memoize]
+fn encode_dp(dps: Vec<DP>, depth: usize) -> usize {
+    let mut total = 0;
     let mut current = DP::A;
     for d in dps {
         let mut alts = match (current, d) {
+            // MATCH-AGEDDON to avoid the corner gap by hand-coding permutations
             (DP::A, DP::A) => vec![vec![]],
             (DP::A, DP::D) => vec![vec![DP::D, DP::L], vec![DP::L, DP::D]],
             (DP::A, DP::L) => vec![vec![DP::D, DP::L, DP::L], vec![DP::L, DP::D, DP::L]], // gap-avoid
@@ -118,17 +86,20 @@ fn encode_dp(dps: Vec<DP>, depth: usize) -> Result<Vec<DP>> {
             (DP::U, DP::U) => vec![vec![]],
         };
         alts.iter_mut().for_each(|alt| alt.push(DP::A));
-        let mut shortest = shortest_encoding(alts, |dps| encode_dp(dps, depth - 1))?;
-        next_dps.append(&mut shortest);
+        if depth == 1 {
+            total += shortest_encoding(alts, |alt| alt.len());
+        } else {
+            total += shortest_encoding(alts, |dps| encode_dp(dps, depth - 1));
+        }
         current = d;
     }
 
-    Ok(next_dps)
+    total
 }
 
-fn route_keypad(kp_keys: &str) -> Result<String> {
+fn route_keypad(kp_keys: &str, max_depth: usize) -> Result<usize> {
     let mut prev = KP_A;
-    let mut seq = String::new();
+    let mut total = 0;
     for key in kp_keys.chars() {
         let to = match key {
             '7' => KP_7,
@@ -152,12 +123,11 @@ fn route_keypad(kp_keys: &str) -> Result<String> {
             .permutations(raw_len)
             .unique()
             .filter(|alt| {
+                let n = alt.len();
                 match (prev.x, prev.y, to.x, to.y) {
-                    (0, _, 1, 3) => alt[alt.len() - 1] != DP::R, // 1st col to 0: last isn't Right
-                    (0, 0, 2, 3) => *alt != vec![DP::D, DP::D, DP::D, DP::R, DP::R], // 7 to A
-                    (0, 1, 2, 3) => *alt != vec![DP::D, DP::D, DP::R, DP::R], // 4 to A
-                    (0, 2, 2, 3) => *alt != vec![DP::D, DP::R, DP::R], // 1 to A
-                    (1 | 2, 3, 0, _) => alt[alt.len() - 1] != DP::U, // 0 or A to 1st col: last isn't Up
+                    (0, _, 1, 3) => alt[n - 1] != DP::R, // 1st col to 0: last isn't Right
+                    (0, _, 2, 3) => !(alt[n - 1] == DP::R && alt[n - 2] == DP::R), // 1st col to A: can't end in R->R
+                    (1 | 2, 3, 0, _) => alt[n - 1] != DP::U, // 0 or A to 1st col: last isn't Up
                     _ => true,
                 }
             })
@@ -166,32 +136,31 @@ fn route_keypad(kp_keys: &str) -> Result<String> {
                 alt
             })
             .collect();
-        println!("{} all: {:?}", key, alts);
-        let shortest = shortest_encoding(alts, |dps| encode_dp(dps, 2))?;
-        println!("{} shortest: {:?}", key, shortest);
-        for d in shortest {
-            seq.push(d.to_char());
-        }
+        total += shortest_encoding(alts, |dps| encode_dp(dps, max_depth));
         prev = to;
     }
 
-    Ok(seq)
+    Ok(total)
 }
 
 pub fn part1(lines: Vec<String>) -> Result<String> {
     let mut total: usize = 0;
     for line in lines {
         let numeric = line.chars().take(3).join("").parse::<usize>()?;
-        let presses = route_keypad(&line)?;
-        println!("{}: {}", line, presses);
-        println!("{}: {} x {}", line, presses.len(), numeric);
-        total += presses.len() * numeric;
+        let presses = route_keypad(&line, 2)?;
+        total += presses * numeric;
     }
     Ok(total.to_string())
 }
 
 pub fn part2(lines: Vec<String>) -> Result<String> {
-    bail!("not done")
+    let mut total: usize = 0;
+    for line in lines {
+        let numeric = line.chars().take(3).join("").parse::<usize>()?;
+        let presses = route_keypad(&line, 25)?;
+        total += presses * numeric;
+    }
+    Ok(total.to_string())
 }
 
 #[cfg(test)]
@@ -204,37 +173,6 @@ mod test {
     fn to_lines(text: &str) -> Vec<String> {
         text.lines().map(|x| x.to_string()).collect()
     }
-
-    // #[test]
-    // fn test_part1_partial_a() -> Result<()> {
-    //     let presses = route_keypad("029A")?;
-    //     assert_eq!(presses_to_str(&presses), "<A^A^^>AvvvA");
-    //     let presses = route_dpad(presses)?;
-    //     assert_eq!(presses_to_str(&presses), "v<<A>>^A<A>A<AAv>A^Av<AAA>^A");
-    //     let presses = route_dpad(presses)?;
-    //     assert_eq!(
-    //         presses_to_str(&presses),
-    //         //  "v   <  < A    >  > ^   A  <    A    >  A  <    A    A v   >  A  ^  A  v   <  A    A A >  ^   A "
-    //         //  "v<A <A A >>^A vA A ^<A >A v<<A >>^A vA ^A v<<A >>^A A v<A >A ^A <A >A v<A <A >>^A A A vA ^<A >A"
-    //         "v<A<AA>>^AvAA^<A>Av<<A>>^AvA^Av<<A>>^AAv<A>A^A<A>Av<A<A>>^AAAvA^<A>A"
-    //     );
-    //     Ok(())
-    // }
-
-    // #[test]
-    // fn test_part1_partial_b() -> Result<()> {
-    //     let presses = route_keypad("379A")?;
-    //     assert_eq!(presses_to_str(&presses), "^A^^<<A>>AvvvA");
-    //     let presses = route_dpad(presses)?;
-    //     assert_eq!(presses_to_str(&presses), "<A>A<AAv<AA>>^AvAA^Av<AAA>^A");
-    //     let presses = route_dpad(presses)?;
-    //     assert_eq!(presses.len(), 64);
-    //     assert_eq!(
-    //         presses_to_str(&presses),
-    //         "v<<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>Av<<A>A>^AAAvA<^A>A"
-    //     );
-    //     Ok(())
-    // }
 
     #[test]
     fn test_part1_a() -> Result<()> {
@@ -266,15 +204,4 @@ mod test {
         assert_eq!(part1(lines)?, "126384");
         Ok(())
     }
-    //OK     ^         A     ^    ^ <   < A    >  > A  v   v v A
-    //OK     <    A    >  A  <  A A v<A A >>^A vA A ^A v<A A A >^A
-    //mine:  v<<A >>^A vA ^A v<<A * >>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A
-    //their: <v<A >>^A vA ^A <vA <A A>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-
-    //OK     <    A    >  A  <    A    A v   <  A    A >  > ^   A  v   A   A ^  A  v   <  A    A A > ^    A
-    //manu:  v<<A >>^A vA ^A v<<A >>^A A v<A <A >>^A A vA A ^<A >A v<A >^A A <A >A v<A <A >>^A A A vA ^<A >A
-
-    //manu:   v<<A>>^AvA^Av<<A>>^AAv<A<A>>^AAvAA^<A>Av<A>^AA<A>Av<A<A>>^AAAvA^<A>A
-    //theirs: <v<A>>^AvA^A<vA<AA>>^AAvA<^A>AAvA^A<vA>^AA<A>A<v<A>A>^AAAvA<^A>A
-    // TODO: decode "theirs" above and see if it outputs 379A. If it does, there's a bug in my algorithm
 }
