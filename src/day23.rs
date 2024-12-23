514e933481268be47a8ce5ca55004da462bfd25c
Jeremy Stashewsky <jstash@gmail.com>
2024-12-22 23:41:21 -0800

    day 23 part 2 in rust

--- src/day23.rs
+++ src/day23.rs
@@ -1,8 +1,7 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashMap, HashSet};
 
-use anyhow::{anyhow, bail, Result};
-
-use graphrs::{algorithms::components, Edge, Graph, GraphSpecs};
+use anyhow::Result;
+use graphrs::{Edge, Graph, GraphSpecs};
 use itertools::Itertools;
 
 use crate::common::*;
@@ -40,7 +39,7 @@ pub fn part1(lines: Vec<String>) -> Result<String> {
             if c.g.get_edge(u.name, v.name).is_ok() {
                 let mut trip_name = vec![t_node, u.name, v.name];
                 trip_name.sort();
-                println!("found {:?}", trip_name);
+                // println!("found {:?}", trip_name);
                 triples.insert(trip_name.join(","));
             }
         }
@@ -48,8 +47,60 @@ pub fn part1(lines: Vec<String>) -> Result<String> {
     Ok(triples.len().to_string())
 }
 
+struct Chal2<'a> {
+    adj: HashMap<&'a str, HashSet<&'a str>>,
+}
+impl<'a> Chal2<'a> {
+    fn parse(lines: &'a Vec<String>) -> Result<Self> {
+        let mut adj = HashMap::new();
+        for line in lines.iter() {
+            let (a, b) = line.split_once('-').unwrap();
+            adj.entry(a).or_insert_with(|| HashSet::new()).insert(b);
+            adj.entry(b).or_insert_with(|| HashSet::new()).insert(a);
+        }
+        Ok(Self { adj })
+    }
+}
+
 pub fn part2(lines: Vec<String>) -> Result<String> {
-    bail!("not done")
+    let c = Chal2::parse(&lines)?;
+    let mut max_clique = "".to_owned();
+    for (root, root_adj) in c.adj.iter() {
+        // Clone a working-set of adjacent nodes
+        let mut work_adj = root_adj.clone();
+        // Go until all neighbours visited
+        while work_adj.len() > 0 {
+            // Clique has just the root initially
+            let mut non_clique: HashSet<&str> = HashSet::new();
+            let mut clique: BTreeSet<&str> = BTreeSet::new();
+            clique.insert(root);
+
+            // Visit each neighb. and see if it's in the clique. If it isn't,
+            // still might be in another clique with this root.
+            for node in work_adj.iter() {
+                let node_adj = c.adj.get(node).unwrap();
+                if clique.iter().all(|n| node_adj.contains(n)) {
+                    clique.insert(node);
+                } else {
+                    non_clique.insert(node);
+                }
+            }
+
+            // Name should already be sorted thanks to BTree
+            let c_name = clique.into_iter().join(",");
+            if c_name.len() > max_clique.len() {
+                max_clique = c_name;
+            }
+
+            work_adj = non_clique;
+
+            // TODO: there's probably some kind of optimization to remove found
+            // cliques from future iterations, but it's fast enough for AoC
+            // as-is.
+        }
+    }
+
+    Ok(max_clique)
 }
 
 #[cfg(test)]
@@ -58,13 +109,8 @@ mod test {
     use anyhow::Result;
     use indoc::indoc;
 
-    fn to_lines(text: &str) -> Vec<String> {
-        text.lines().map(|x| x.to_string()).collect()
-    }
-
-    #[test]
-    fn test_part1() -> Result<()> {
-        let lines = to_lines(indoc! {"
+    fn input() -> Vec<String> {
+        let text = indoc! {"
             kh-tc
             qp-kh
             de-cg
@@ -97,8 +143,19 @@ mod test {
             wh-qp
             tb-vc
             td-yn
-        "});
-        assert_eq!(part1(lines)?, "7");
+        "};
+        text.lines().map(|x| x.to_string()).collect()
+    }
+
+    #[test]
+    fn test_part1() -> Result<()> {
+        assert_eq!(part1(input())?, "7");
+        Ok(())
+    }
+
+    #[test]
+    fn test_part2() -> Result<()> {
+        assert_eq!(part2(input())?, "co,de,ka,ta");
         Ok(())
     }
 }
